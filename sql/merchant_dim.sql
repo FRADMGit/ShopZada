49e95b508d7edda9bc5d69bfb2a4b52aa9d507e5
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-30 08:25:40 +0800

    Update merchant_dim.sql

--- sql/merchant_dim.sql
+++ sql/merchant_dim.sql
@@ -1,1 +1,11 @@
-SELECT DISTINCT * FROM {{ ref('stg_merchant_data') }}
+SELECT *
+FROM (
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY merchant_id, merchant_name
+            ORDER BY merchant_creation_datetime DESC NULLS LAST
+        ) AS rn
+    FROM {{ ref("stg_merchant_data") }}
+)
+WHERE rn = 1
