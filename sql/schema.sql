6d398d75648e16728ced2a6c1158b3385136360c
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-12-04 11:44:26 +0800

    simplified now that ids are pks

--- sql/schema.sql
+++ sql/schema.sql
@@ -19,25 +19,7 @@
 -- MERCHANT DIM
   DROP TABLE IF EXISTS merchant_dim CASCADE;
   CREATE TABLE merchant_dim AS
-  SELECT
-      merchant_id,
-      merchant_creation_datetime,
-      merchant_name,
-      merchant_street,
-      merchant_state,
-      merchant_city,
-      merchant_country,
-      merchant_contact_number
-  FROM (
-      SELECT
-          *,
-          ROW_NUMBER() OVER (
-              PARTITION BY merchant_id, merchant_name
-              ORDER BY merchant_creation_datetime DESC NULLS LAST
-          ) AS rn
-      FROM clean_merchant_data
-  )
-  WHERE rn = 1;
+  SELECT * FROM clean_merchant_data;
 -- ORDER DIM
   DROP TABLE IF EXISTS order_dim CASCADE;
   CREATE TABLE order_dim AS
@@ -52,79 +34,9 @@
           b.order_arrival_days
       FROM clean_order_with_merchant_data a
       JOIN clean_order_data b ON a.order_id = b.order_id
-      JOIN clean_user_data c ON b."user_id" = c."user_id"
-      JOIN clean_transactional_campaign_data d ON b.order_id = d.order_id
-  ),
-  correct_user AS (
-      SELECT DISTINCT
-          order_id,
-          "user_id",
-          "user_name"
-      FROM (
-          SELECT
-              o.order_id,
-              u."user_id",
-              u."user_name",
-              u.user_creation_datetime,
-              o.order_transaction_date,
-              ROW_NUMBER() OVER (
-                  PARTITION BY o.order_id
-                  ORDER BY u.user_creation_datetime DESC
-              ) AS rn
-          FROM clean_order_data o
-          JOIN clean_user_data u
-              ON o."user_id" = u."user_id"
-              AND u.user_creation_datetime <= o.order_transaction_date
-      ) x
-      WHERE rn = 1
-  ),
-  correct_merchant AS (
-      SELECT DISTINCT
-          order_id,
-          merchant_id,
-          merchant_name
-      FROM (
-          SELECT
-              a.order_id,
-              m.merchant_id,
-              m.merchant_name,
-              m.merchant_creation_datetime,
-              a.order_transaction_date,
-              ROW_NUMBER() OVER (
-                  PARTITION BY a.order_id
-                  ORDER BY m.merchant_creation_datetime DESC
-              ) AS rn
-          FROM affiliations a
-          JOIN clean_merchant_data m
-              ON a.merchant_id = m.merchant_id
-              AND m.merchant_creation_datetime <= a.order_transaction_date
-      ) x
-      WHERE rn = 1
-  ),
-  correct_staff AS (
-      SELECT DISTINCT
-          order_id,
-          staff_id,
-          staff_name
-      FROM (
-          SELECT
-              a.order_id,
-              s.staff_id,
-              s.staff_name,
-              s.staff_creation_datetime,
-              a.order_transaction_date,
-              ROW_NUMBER() OVER (
-                  PARTITION BY a.order_id
-                  ORDER BY s.staff_creation_datetime DESC
-              ) AS rn
-          FROM affiliations a
-          JOIN clean_staff_data s
-              ON a.staff_id = s.staff_id
-              AND s.staff_creation_datetime <= a.order_transaction_date
-      ) x
-      WHERE rn = 1
+      LEFT JOIN clean_transactional_campaign_data d ON b.order_id = d.order_id
   )
-  SELECT DISTINCT
+  SELECT
       a.order_id,
       u."user_id",
       u."user_name",
@@ -137,9 +49,9 @@
       a.order_arrival_days,
       d.order_delay_days
   FROM affiliations a
-  JOIN correct_user u ON a.order_id = u.order_id
-  JOIN correct_merchant m ON a.order_id = m.order_id
-  JOIN correct_staff s ON a.order_id = s.order_id
+  JOIN clean_user_data u ON a."user_id" = u."user_id"
+  JOIN clean_merchant_data m ON a.merchant_id = m.merchant_id
+  JOIN clean_staff_data s ON a.staff_id = s.staff_id
   JOIN clean_order_delays d ON a.order_id = d.order_id;
 -- PRODUCT DIM
   DROP TABLE IF EXISTS product_dim CASCADE;
@@ -151,53 +63,28 @@
   SELECT DISTINCT * FROM clean_staff_data;
 -- USER DIM
   DROP TABLE IF EXISTS user_dim CASCADE;
+
   CREATE TABLE user_dim AS
-  WITH ranked_users AS (
-      SELECT
-          ud."user_id",
-          ud."user_name",
-          ud.user_creation_datetime,
-          ud.user_street,
-          ud.user_state,
-          ud.user_city,
-          ud.user_country,
-          ud.user_birth_datetime,
-          ud.user_gender,
-          ud.user_device_address,
-          ud.user_type,
-          cc.name AS credit_card_holder_name,
-          cc.credit_card_number,
-          cc.issuing_bank,
-          job.user_job,
-          job.user_job_level,
-          ROW_NUMBER() OVER (
-              PARTITION BY ud."user_id"
-              ORDER BY ud.user_creation_datetime DESC
-          ) AS rn
-      FROM clean_user_data AS ud
-      JOIN clean_user_credit_card AS cc 
-          ON ud."user_id" = cc."user_id"
-      JOIN clean_user_job AS job 
-          ON ud."user_id" = job."user_id"
-      WHERE ud."user_id" IS NOT NULL
-        AND ud."user_name" IS NOT NULL
-  )
   SELECT
-      "user_id",
-      "user_name",
-      user_creation_datetime,
-      user_street,
-      user_state,
-      user_city,
-      user_country,
-      user_birth_datetime,
-      user_gender,
-      user_device_address,
-      user_type,
-      credit_card_holder_name,
-      credit_card_number,
-      issuing_bank,
-      user_job,
-      user_job_level
-  FROM ranked_users
-  WHERE rn = 1
+      u."user_id",
+      u."user_name",
+      u.user_creation_datetime,
+      u.user_street,
+      u.user_state,
+      u.user_city,
+      u.user_country,
+      u.user_birth_datetime,
+      u.user_gender,
+      u.user_device_address,
+      u.user_type,
+      cc.name AS credit_card_holder_name,
+      cc.credit_card_number,
+      cc.issuing_bank,
+      job.user_job,
+      job.user_job_level
+  FROM clean_user_data u
+  JOIN clean_user_credit_card cc ON u."user_id" = cc."user_id"
+  JOIN clean_user_job job ON u."user_id" = job."user_id"
+  WHERE u."user_id" IS NOT NULL
+    AND u."user_name" IS NOT NULL;
+
